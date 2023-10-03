77b59a38cae3f4b99936c8af24451e07abc5ce9e
JuliaYarosheva <58659845+JuliaYarosheva@users.noreply.github.com>
2023-10-03 16:18:53 +0300

    Fix path

--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -17,19 +17,19 @@ export const footerDetailsItems = [
 
 export const sliderItems = [
   {
-    img: `${import.meta.env.BASE_URL}public/images/slider-img-1.png`,
+    img: `${import.meta.env.BASE_URL}images/slider-img-1.png`,
     caption: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
   },
   {
-    img: `${import.meta.env.BASE_URL}public/images/slider-img-2.png`,
+    img: `${import.meta.env.BASE_URL}images/slider-img-2.png`,
     caption: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
   },
   {
-    img: `${import.meta.env.BASE_URL}public/images/slider-img-3.png`,
+    img: `${import.meta.env.BASE_URL}images/slider-img-3.png`,
     caption: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
   },
   {
-    img: `${import.meta.env.BASE_URL}public/images/slider-img-4.png`,
+    img: `${import.meta.env.BASE_URL}images/slider-img-4.png`,
     caption: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
   },
 ];
