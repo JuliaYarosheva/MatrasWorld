91da3c7471bc1afa21e884bc593db0f86067709b
JuliaYarosheva <58659845+JuliaYarosheva@users.noreply.github.com>
2023-10-10 00:09:51 +0300

    Add mobile nav

--- src/components/navigation/MobileNavigation.jsx
+++ src/components/navigation/MobileNavigation.jsx
@@ -1,11 +1,19 @@
+import { slide as Menu } from "react-burger-menu";
+import HamburgerIcon from "./HamburgerIcon";
 import NavMenuContent from "./NavMenuContent";
 
 const MobileNavigation = ({ styles }) => {
   return (
     <div className={styles.hamburger}>
-      <div>
-        <NavMenuContent styles={styles} />
-      </div>
+      <Menu
+        customBurgerIcon={<HamburgerIcon styles={styles} />}
+        width={"75%"}
+        className={styles.hamburgerPosition}
+      >
+        <div>
+          <NavMenuContent styles={styles} />
+        </div>
+      </Menu>
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+function isExternal(id: string) {
+  return !id.startsWith(".") && !path.isAbsolute(id) && !id.startsWith("~/");
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    external: isExternal,
+  },
 });
