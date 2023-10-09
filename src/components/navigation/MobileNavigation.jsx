638fdf851d572deedfa9719b19ac396c6402c925
JuliaYarosheva <58659845+JuliaYarosheva@users.noreply.github.com>
2023-10-10 00:03:31 +0300

    remove mobile nav

--- src/components/navigation/MobileNavigation.jsx
+++ src/components/navigation/MobileNavigation.jsx
@@ -1,19 +1,11 @@
-import { slide as Menu } from "react-burger-menu";
-import HamburgerIcon from "./HamburgerIcon";
 import NavMenuContent from "./NavMenuContent";
 
 const MobileNavigation = ({ styles }) => {
   return (
     <div className={styles.hamburger}>
-      <Menu
-        customBurgerIcon={<HamburgerIcon styles={styles} />}
-        width={"75%"}
-        className={styles.hamburgerPosition}
-      >
-        <div>
-          <NavMenuContent styles={styles} />
-        </div>
-      </Menu>
+      <div>
+        <NavMenuContent styles={styles} />
+      </div>
     </div>
   );
 };
